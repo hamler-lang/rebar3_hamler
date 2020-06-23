212cf8ac1700571f594e6aa291cf20f75f08966e
terry-xiaoyu <506895667@qq.com>
2020-06-23 10:57:21 +0800

    fix(path): got incorrect root dir of hamler project

--- src/rebar3_hamler.erl
+++ src/rebar3_hamler.erl
@@ -17,18 +17,27 @@ find_hamler_paths(State) ->
                    lib_path(State)],
     ?LOG(debug, "find hamler path in ~p", [SearchPaths]),
     HmDirs = lists:append([filelib:wildcard(Dir) || Dir <- SearchPaths]),
-    lists:usort([filename:dirname(filename:dirname(Dir))
-                 || Dir <- HmDirs]).
+    lists:usort([extract_project_path(Dir) || Dir <- HmDirs]).
 
 src_path(State) ->
-    filename:join([rebar_dir:root_dir(State), "src", "*.hm"]).
+    filename:join([rebar_dir:root_dir(State), "src", "**", "*.hm"]).
 
 deps_path(State) ->
-    filename:join([rebar_dir:deps_dir(State), "*", "src", "*.hm"]).
+    filename:join([rebar_dir:deps_dir(State), "*", "src", "**", "*.hm"]).
 
 apps_path(State) ->
-    filename:join([rebar_dir:root_dir(State), "apps", "*", "src", "*.hm"]).
+    filename:join([rebar_dir:root_dir(State), "apps", "*", "src", "**", "*.hm"]).
 
 lib_path(State) ->
-    filename:join([rebar_dir:root_dir(State), "lib", "*", "src", "*.hm"]).
+    filename:join([rebar_dir:root_dir(State), "lib", "*", "src", "**", "*.hm"]).
+
+%% the project dir is the parent dir of "src"
+extract_project_path(Dir) ->
+    Basename = filename:basename(Dir),
+    extract_project_path(Dir, Basename).
+
+extract_project_path(Dir, "src") ->
+    filename:dirname(Dir);
+extract_project_path(Dir, _Basename) ->
+    extract_project_path(filename:dirname(Dir)).
 
